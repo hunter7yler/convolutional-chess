88280c295f6dd52861812cf516b6490fbe4d3526
hunter7yler <hunter7yler@gmail.com>
2021-01-10 18:07:11 -0700

    Adds readme, sample data, and board->move CNN chess AI.

--- local_engines.py
+++ local_engines.py
@@ -39,7 +39,7 @@ from tensorflow import keras
 #     choice = scored[0][1]
 #     return choice
 
-class HighestValue_OR_NN(BaseEngine):
+class HighestValueCNN(BaseEngine):
     """ Engine that prioritizes capturing the highest value piece if
     the option presents itself, uses a neural network to choose between equally valued moves"""
 
@@ -93,3 +93,49 @@ class HighestValue_OR_NN(BaseEngine):
                 )
 
         return highest_capture_uci
+
+
+class CNN(BaseEngine):
+    """ Uses a CNN to choose next move """
+
+    def __init__(self):
+        """ See parent docstring """
+        super().__init__()
+        self.name = "Uses a CNN to choose next move"
+        self.start_nn = keras.models.load_model('./data/start_white')
+        self.end_nn = keras.models.load_model('./data/end_white')
+
+    def evaluate(self, board: chess.Board) -> None:
+        """ Assigns highest value to capture moves based off value system """
+        self.reset_move_variables()
+
+        bint = np.ndarray(shape=(1, 8, 8, 12))
+        bint[0] = board_convert(str(board))
+        s_array, e_array = self.start_nn(bint).numpy()[0], self.end_nn(bint).numpy()[0]
+
+        legal_move_list = list(board.legal_moves)
+        for m in legal_move_list:
+            self.legal_moves[m] = 0.0 + s_array[move_index(str(m)[0:2])] #+ (e_array[move_index(str(m)[2:4])])
+
+        self.material_difference.append(
+            self.evaluation_function.evaluate(board)
+        )
+
+
+    def move(self, board: chess.Board) -> chess.Move:
+        """Select move that based on CNN applied score"""
+        self.evaluate(board)
+
+        if not [*self.legal_moves]:
+            return chess.Move.null()
+
+        highest_val, highest_uci = 0.0, None
+
+        for m in list(self.legal_moves):
+            if self.legal_moves[m] > highest_val:
+                highest_capture_val, highest_uci = (
+                    self.legal_moves[m],
+                    m,
+                )
+
+        return highest_uci
---
+++ readme.md
@@ -0,0 +1,39 @@
+# CNN Chess Engine
+
+This project tests the efficacy of Convolutional Neural Networks (CNNs) in Chess AIs.
+
+##Install and setup
+
+
+
+### Prerequisites
+
+This project uses Python 3. All other dependencies are listed in requirements.txt. To get a virtualenv set up for this project, run the following three commands from the nn_chess directory:
+
+Create a virtualenv:
+
+```
+python3 -m venv ./nnc_env
+```
+Activate the virtualenv:
+```
+source ./nnc_env
+```
+Install all dependencies:
+```
+pip install -r requirements.txt
+```
+
+### CNN Training
+
+Replace the sample file nn_chess/data/data.pgn with any data you like. I recommend a year's worth of data from the [FICS Games Database](https://www.ficsgames.org/download.html). 
+
+Now you can train your CNN by executing the following: 
+```
+python nn_gen.py
+```
+This will save two CNNs into nn_chess/data, one for recommending what piece to move and another for deciding where to put it.
+
+### Run Simulations
+
+Once you've trained your CNNs, you can test the AIs against the other basic chess engines using Jupyter Notebook. The cells in local_engines.py are examples tests comparing the CNN AIs against the basic chess engines from the Chessmate Python module.
